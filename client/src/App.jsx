ce05acd8848c552d696e35732179ada939f62fa3
SoniaMEGS <megs.sonia@gmail.com>
2024-01-20 18:30:37 -0800

    The api was linked to the app, and components were created  to render the list of dogs

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,35 +1,38 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import React from "react";
+import Searcher from "./components/Searcher";
+import Cards from "./components/Cards";
+import Loader from "./components/Loader.jsx";
+import { getDog } from "./api/index";
+import { setDogs, setLoading } from "./redux/actions/actions.js";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const dogs = useSelector((state) => state.dogs);
+  const loading = useSelector((state) => state.loading);
+  const dispatch = useDispatch();
+  //const [dogs, setDogs] = useState([]);
+
+  //Funcion que llama la api
+  useEffect(() => {
+    const fetchDogs = async () => {
+      dispatch(setLoading(true));
+      const dogsRes = await getDog();
+      dispatch(setDogs(dogsRes));
+      dispatch(setLoading(false));
+    };
+    fetchDogs();
+  }, []);
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div>
+      <div className="app">
+        <Searcher />
+        {loading ? <Loader /> : <Cards dogs={dogs} />}
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
